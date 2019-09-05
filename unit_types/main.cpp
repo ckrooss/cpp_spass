7ba1ebd21e8f185f2d3f11309d01dc6c5027ec47
Christopher Krooß <c.krooss@gmail.com>
2019-09-05 21:55:15 +0200

    add map example

--- unit_types/main.cpp
+++ unit_types/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <map>
+#include <unordered_map>
 
 template<typename T, typename P>
 class Container {
@@ -19,19 +21,23 @@ public:
         return *this;
     }
 
-    bool operator>(const T& value) {
+    bool operator!=(const T& value) const {
+        return m_value != value;
+    }
+
+    bool operator>(const T& value) const {
         return m_value > value;
     }
 
-    bool operator<(const T& value) {
+    bool operator<(const T& value) const {
         return m_value < value;
     }
 
-    bool operator==(const T& value) {
+    bool operator==(const T& value) const {
         return m_value == value;
     }
 
-    Container operator+(const T& value) {
+    Container operator+(const T& value) const {
         return {m_value + value};
     }
 
@@ -45,19 +51,23 @@ public:
         return *this;
     }
 
-    bool operator>(const P& other) {
+    bool operator!=(const P& other) const {
+        return m_value != other.m_value;
+    }
+
+    bool operator>(const P& other) const {
         return m_value > other.m_value;
     }
 
-    bool operator<(const P& other) {
+    bool operator<(const P& other) const {
         return m_value < other.m_value;
     }
 
-    bool operator==(const P& other) {
+    bool operator==(const P& other) const {
         return m_value == other.m_value;
     }
 
-    Container operator+(const P& other) {
+    Container operator+(const P& other) const {
         return {m_value + other.m_value};
     }
 
@@ -70,6 +80,7 @@ public:
         return ++m_value;
     }
 
+//Other stuff
     T get() const {
         return m_value;
     }
@@ -82,6 +93,14 @@ public:
         T m_value;
 };
 
+// Hash function is needed to use Container as a key for std::unordered_map
+struct ContainerHash {
+template<typename T, typename P>
+  std::size_t operator()(const Container<T, P>& container) const {
+    return std::hash<T>()(container.get());
+  }
+};
+
 struct Seconds : public Container<time_t, Seconds> {
     Seconds(value_type value) : Container(value) {};
     std::string getUnit() const {
@@ -89,11 +108,17 @@ struct Seconds : public Container<time_t, Seconds> {
     }
 };
 
-
 struct Kph : public Container<int, Kph> {
     Kph(value_type value) : Container(value) {};
     std::string getUnit() const {
         return "km/h";
+    }
+};
+
+struct Risk : public Container<int, Risk> {
+    Risk(value_type value) : Container(value) {};
+    std::string getUnit() const {
+        return "risk";
     }
 };
 
@@ -107,17 +132,35 @@ struct Meters : public Container<int, Meters> {
     }
 };
 
-
-//Seconds time_for_distance_at_speed();
+using speed_to_risk_t = std::unordered_map<Kph, Risk, ContainerHash>;
+static speed_to_risk_t speed_to_risk = {
+    {1, 0},
+    {2, 0},
+    {3, 0},
+    {4, 0},
+    {5, 1},
+    {6, 2},
+    {7, 3},
+    {8, 4},
+    {9, 5},
+    {10, 7}
+};
 
 int main() {
     Meters x{800};
-    Kph y{60};
+    Kph y{9};
+
+    std::cout << x << " / " << y << " = " << x / y << std::endl;
+
+    std::cout << "at v=" << y << " the relative risk is " << speed_to_risk.at(y) << std::endl;
+
 
     // error: invalid operands to binary expression ('Kph' and 'Meters')
     // std::cout << x << " + " << y << " = " << x + y << std::endl;
 
-    std::cout << x << " / " << y << " = " << x / y << std::endl;
+    // error: no matching member function for call to 'at'
+    // no known conversion from 'Meters' to 'Kph' for 1st argument
+    // std::cout << "at x=" << x<< " the relative risk is " << speed_to_risk.at(x) << std::endl;
 
     return 0;
 }
