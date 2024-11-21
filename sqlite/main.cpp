b75fad68a374b604b0376168e654854616220a16
Christopher Krooß <c.krooss@mailbox.org>
2024-11-21 19:40:16 +0100

    update

--- sqlite/main.cpp
+++ sqlite/main.cpp
@@ -5,18 +5,36 @@
 #include <cerrno>
 #include <cstring>
 
+#include <SQLiteCpp/SQLiteCpp.h>
+
 int cb(void* /*none*/, int /*cols*/, char** /*column_text*/, char** /*column_names*/) {
     std::cout << "yay\n";
     return 0;
 }
 
-int _main() {
+int main_cpp() {
+    SQLite::Database db("/tmp/mydb.db", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+    SQLite::Statement creat(db, "CREATE TABLE IF NOT EXISTS users (id int, name varchar(255));");
+    creat.exec();
+
+    SQLite::Statement insert(db, "INSERT INTO users VALUES (?, ?);");
+    insert.bind(1, 1);
+    insert.bind(2, "Hans");
+    if(insert.exec() != 1) {
+        printf("Failed to insert\n");
+        return 1;
+    }
+
+    return 0;
+}
+
+int main_c() {
     sqlite3* db;
     char* err = nullptr;
     auto retval = sqlite3_open("/tmp/mydb.db", &db);
     if (retval)
         printf("Could not open db!\n");
-    //retval = sqlite3_exec(db, "CREATE TABLE users (id int, name varchar(255));", cb, nullptr, &err);c
+    retval = sqlite3_exec(db, "CREATE TABLE users (id int, name varchar(255));", cb, nullptr, &err);
     retval = sqlite3_exec(db, "INSERT INTO users ());", cb, nullptr, &err);
     if (retval)
         printf("Could not create table: %s\n", err);
@@ -28,15 +46,10 @@ int _main() {
 }
 
 int main() {
-    FILE* fd;
-    const char* new_err{"OH HAI"};
-    if (nullptr == (fd = fopen("/asd", "r"))) {
-        printf("%s\n", strerror(errno));
-        printf("%s\n", strerror(errno));
-        memcpy(strerror(errno), new_err, 7);
-        printf("%s\n", strerror(errno));
-        printf("%s\n", strerror(errno));
-        exit(1);
+    try{
+    return main_cpp();
     }
-    return 0;
+    catch(const SQLite::Exception& e) {
+        printf("Error in sqlite: %s", e.what());
+    }
 }
